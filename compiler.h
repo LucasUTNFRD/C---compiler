e26813fdea81e659fe32e0691f43ee3372fb6bb2
lucas <ldelgado@frd.utn.edu.ar>
2024-03-14 13:52:37 -0300

    add token structs

--- compiler.h
+++ compiler.h
@@ -2,6 +2,47 @@
 #define COMPILERPROJECT_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+  int line;
+  int col;
+  const char * filename;
+};
+
+enum
+{
+  TOKEN_TYPE_IDENTIFIER,
+  TOKEN_TYPE_KEYWORD,
+  TOKEN_TYPE_OPERATOR,
+  TOKEN_TYPE_SYMBOL,
+  TOKEN_TYPE_NUMBER,
+  TOKEN_TYPE_STRING,
+  TOKEN_TYPE_COMMENT,
+  TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+  int type;
+  int flags;
+
+  union
+  {
+    char charValue;
+    char *stringValue;
+    unsigned int inum;
+    unsigned long lnum;
+    unsigned long long llnum;
+    void * any;
+  };
+
+  bool whitespace;
+
+  const char* between_brackets;
+};
+
 
 enum
 {
